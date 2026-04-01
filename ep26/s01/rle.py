92576da8c9bcd9eb7e7ef07734f525c71b4e340d
MoonsieurNSI <patrice.nicolas7@monlycee.net>
2026-04-01 07:44:53 +0000

    s01 fini

--- ep26/s01/rle.py
+++ ep26/s01/rle.py
@@ -1,9 +1,8 @@
 from PIL import Image
 
+
 def codage_rle(liste_octets):
     '''Renvoie une liste d'octets obtenue par compression RLE'''
-    #changement
-    #lisPillowte_rle = [] 
     liste_rle = []
     i = 0
     while i < len(liste_octets):
@@ -11,6 +10,7 @@ def codage_rle(liste_octets):
         k = 1
         while i+k < len(liste_octets) and liste_octets[i+k] == c:
             k += 1
+        print(k)
         liste_rle.append(k)
         liste_rle.append(c)
         i += k
@@ -33,23 +33,25 @@ def test_codage():
     assert decodage_rle([1, 9, 1, 250, 1, 128]) == [9, 250, 128]
     assert decodage_rle([1, 0]) == [0]
     print("bvo")
-#############################################################################
-# Il n'est pas nécessaire de comprendre le code de ces 4 fonctions, mais il # 
-# sera nécessaire de les utiliser dans la suite à partir de l'exemple.      #
-#############################################################################
 
 def enregistrer_octets(nom_fichier, liste_octets):
     '''Enregistre une liste de valeurs numériques entre 0 et 255 dans un
     le fichier nom_fichier. Si une valeur est plus grande que 255 on considère
     que c'est 255. De même pour les valeur plus petite que 0.'''
+    # Le fichier est ouvert en mode binaire pour pouvoir écrire sans restriction toute valeur
+    # entre 0 et 255
     with open(nom_fichier, 'wb') as fichier:
-        fichier.write(bytes([ max(0, min(255, b)) for b in liste_octets]))
+        # Pour convertir une liste de valeur entre 0 et 255 en une liste d'octets qui peuvent
+        # être écrits dans le fichier, on utilise `bytes`
+        fichier.write(bytes([max(0, min(255, b)) for b in liste_octets]))
+
 
 def charger_octets(nom_fichier):
     '''Renvoie la liste des octets présents dans le fichier nom_fichier'''
     with open(nom_fichier, 'rb') as fichier:
         liste_octets = list(fichier.read())
         return liste_octets
+
 
 def enregistrer_image(nom_image, largeur, liste_niveaux):
     '''Enregistre un fichier image nom_image de la largeur donnée et dont les 
@@ -58,6 +60,7 @@ def enregistrer_image(nom_image, largeur, liste_niveaux):
     hauteur = len(liste_niveaux) // largeur
     im = Image.frombytes('L', (largeur, hauteur), bytes(liste_niveaux))
     im.save(nom_image)
+
 
 def charger_image(nom_image):
     '''Étant donné une image nom_image, renvoie un couple (largeur, liste_niveaux) où 
@@ -71,16 +74,37 @@ def charger_image(nom_image):
 # Fonction nécessaire pour les tests de la question 3                       #
 #############################################################################
 
+
 def encoder_decoder_image(nom_image):
     '''Fonction de test permettant d'encoder puis décoder une image avec un
     codage RLE. Le fichier rle est nommé nom_image.rle et le fichier decodé
     est nom_image.dec.png'''
     w, l = charger_image(nom_image)
-    enregistrer_octets(nom_image+'.rle', codage_rle(l))
+    print(w)
+    enregistrer_octets(nom_image+'.rle', codage_rleV2(l))
     l = charger_octets(nom_image+'.rle')
     enregistrer_image(nom_image+'.dec.png', w, decodage_rle(l))
-
-
+#############################
+def codage_rleV2(liste_octets):
+    '''Renvoie une liste d'octets obtenue par compression RLE'''
+    liste_rle = []
+    i = 0
+    while i < len(liste_octets):
+        c = liste_octets[i]
+        k = 1
+        while i+k < len(liste_octets) and liste_octets[i+k] == c:
+            k += 1
+        q = k // 255               # combien de fois 255 pixels identiques
+        r = k % 255                # plus eventuellement un petit reste < 255
+        for _ in range(q):         # q fois
+            liste_rle.append(255)  # on ajoute le "couple"
+            liste_rle.append(c)    # 255 fois c
+        if r != 0:                 # on ajoute r fois c
+            liste_rle.append(r)    
+            liste_rle.append(c)
+        i += k
+    return liste_rle
+#############################
 
 #tests
 #q1
@@ -92,3 +116,19 @@ print(codage_rle([0]))
 print(decodage_rle([1, 0]))
 test_codage()
 
+#q3
+#encoder_decoder_image("./ep26/s01/bac_nsi_32.png")
+#tout est ok
+encoder_decoder_image("./ep26/s01/bac_nsi_256.png")
+#le fichier bac_nsi_256.png.dec.png ne correspond pas à l'original
+
+#q4
+# le problème qui intervient sur "les gros fichiers" proviendrait des grosses images
+# la fonction  'encoder_decoder_image' commence par appeler 'charger_image'
+# cette dernière n'est pas à comprendre....donc le pb ne vient pas de là
+# le texte invite à modifier 'codage_rle' et 'decodage_rle'
+# le décodage ne doit pas poser de problème...
+# dans le codage on compte combien de paquets de 255 bits identiques on a à la suite
+# On ajoute alors les paquets
+
+# on modifie la ligne 84 pour utiliser la version 2 de la fonction
